365a7821545cae1c1967f71a88cfbfc2d2b8332d
vsviveksolanki2003 <vsviveksolanki@gmail.com>
2026-03-14 11:47:14 +0530

    Time: 0 ms (100%), Space: 9.6 MB (99.96%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,37 @@
 class Solution {
-    private:
-       void solve(vector<int>& nums,vector<int> output,int indx,vector<vector<int>>& ans)
-       { //base case
-         if(indx>=nums.size())
-          {  ans.push_back(output);
-            return;
-          }
-            //exclude case
-            solve(nums,output,indx+1,ans);
+//     private:
+//        void solve(vector<int>& nums,vector<int> output,int indx,vector<vector<int>>& ans)
+//        { //base case
+//          if(indx>=nums.size())
+//           {  ans.push_back(output);
+//             return;
+//           }
+//             //exclude case
+//             solve(nums,output,indx+1,ans);
 
-            //include case
-            int element=nums[indx];
-            output.push_back(element);
-            solve(nums,output,indx+1,ans);
-       }
-public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> output;
-        solve(nums,output,0,ans);
-        return ans;
-    }
+//             //include case
+//             int element=nums[indx];
+//             output.push_back(element);
+//             solve(nums,output,indx+1,ans);
+//        }
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> output;
+//         solve(nums,output,0,ans);
+//         return ans;
+
+  //BIT MANUPULATION TECHNIQUE
+     public: 
+         vector<vector<int>> subsets(vector<int>& nums)
+         {
+            int n=nums.size(), p=1<<n;
+            vector<vector<int>> subs(p);
+            for(int i=0;i<p;i++)
+              for(int j=0;j<n;j++)
+                 if((i>>j)&1)
+                    subs[i].push_back(nums[j]);
+                return subs;
+         }
+    
 };
